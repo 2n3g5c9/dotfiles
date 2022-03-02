b0b9908ef35550af4c593074db4cb8aac2f6d2fe
Marc Molina <marc.m@outlook.com>
2022-03-02 14:28:37 +0000

    :heavy_minus_sign: Remove manually installed LSPs

--- run_once_before_install.sh
+++ run_once_before_install.sh
@@ -4,5 +4,3 @@ PACKER_DIRECTORY="$HOME/.local/share/nvim/site/pack/packer/start/packer.nvim"
 if [ ! -d "$PACKER_DIRECTORY" ]; then
     git clone --depth 1 https://github.com/wbthomason/packer.nvim $PACKER_DIRECTORY
 fi
-
-npm i -g ansible-language-server bash-language-server dockerfile-language-server-nodejs pyright @tailwindcss/language-server typescript typescript-language-server vim-language-server vscode-langservers-extracted yaml-language-server
