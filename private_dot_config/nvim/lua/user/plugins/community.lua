fcbd280244a26ffdb93ed983c7ab8833b4b25689
Marc Molina <marc.m@outlook.com>
2023-10-22 21:10:53 +0100

    Update AstroNvim config

---
+++ private_dot_config/nvim/lua/user/plugins/community.lua
@@ -0,0 +1,7 @@
+return {
+  "AstroNvim/astrocommunity",
+  { import = "astrocommunity.bars-and-lines.smartcolumn-nvim" },
+  { import = "astrocommunity.colorscheme.catppuccin" },
+  { import = "astrocommunity.completion.copilot-lua" },
+  { import = "astrocommunity.pack.go" },
+}
