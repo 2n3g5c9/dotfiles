2653c87623883ba90b5a63c9bce047b079a148cf
Marc Molina <marc.m@outlook.com>
2023-10-22 21:37:24 +0100

    Add Neovim git plugins

--- private_dot_config/nvim/lua/user/plugins/community.lua
+++ private_dot_config/nvim/lua/user/plugins/community.lua
@@ -3,5 +3,9 @@ return {
   { import = "astrocommunity.bars-and-lines.smartcolumn-nvim" },
   { import = "astrocommunity.colorscheme.catppuccin" },
   { import = "astrocommunity.completion.copilot-lua" },
+  { import = "astrocommunity.git.blame-nvim" },
+  { import = "astrocommunity.git.diffview-nvim" },
   { import = "astrocommunity.pack.go" },
+  { import = "astrocommunity.programming-language-support.csv-vim" },
+  { import = "astrocommunity.programming-language-support.web-tools-nvim" },
 }
