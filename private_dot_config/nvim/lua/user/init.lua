6f663af8f78a4035a3f572215cdd4d1650e24e46
Marc Molina <marc.m@outlook.com>
2023-10-22 21:23:31 +0100

    Update Alacritty config

--- private_dot_config/nvim/lua/user/init.lua
+++ private_dot_config/nvim/lua/user/init.lua
@@ -1,5 +1,12 @@
 vim.g.catppuccin_flavour = "mocha"
 
+vim.cmd[[augroup vimbrant]]
+vim.cmd[[autocmd!]]
+vim.cmd[[autocmd ColorScheme * highlight Normal guibg=NONE ctermbg=NONE]]
+vim.cmd[[autocmd ColorScheme * highlight NonText guibg=NONE ctermbg=NONE]]
+vim.cmd[[augroup END]]
+
+
 return {
   colorscheme = "catppuccin",
 }
