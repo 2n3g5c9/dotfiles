a1a9bad8fd9e41c9cd99f86ab9ffdb59c740f941
Marc Molina <marc.m@outlook.com>
2023-11-25 22:54:30 +0000

    Add .config/fish/conf.d/infrastructure.fish

---
+++ private_dot_config/fish/conf.d/infrastructure.fish
@@ -0,0 +1,5 @@
+# Google Cloud SDK
+source /opt/homebrew/Caskroom/google-cloud-sdk/latest/google-cloud-sdk/path.fish.inc
+
+# Kubernetes
+test -f ~/.kubectl_aliases.fish && source ~/.kubectl_aliases.fish
