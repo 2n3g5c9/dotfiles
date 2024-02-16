38f8c95b1e97fbda2aed0641196c4261dbf7585a
Marc Molina <marc.m@outlook.com>
2024-02-16 20:23:36 +0000

    Use the copilot-lua-cmp for GitHub Copilot

--- private_dot_config/nvim/lua/user/plugins/community.lua
+++ private_dot_config/nvim/lua/user/plugins/community.lua
@@ -2,11 +2,12 @@ return {
   "AstroNvim/astrocommunity",
   { import = "astrocommunity.bars-and-lines.smartcolumn-nvim" },
   { import = "astrocommunity.colorscheme.catppuccin" },
-  { import = "astrocommunity.completion.copilot-lua" },
+  { import = "astrocommunity.completion.copilot-lua-cmp" },
   { import = "astrocommunity.git.blame-nvim" },
   { import = "astrocommunity.git.diffview-nvim" },
   { import = "astrocommunity.pack.go" },
   { import = "astrocommunity.pack.rust" },
+  { import = "astrocommunity.pack.swift" },
   { import = "astrocommunity.programming-language-support.csv-vim" },
   { import = "astrocommunity.programming-language-support.web-tools-nvim" },
 }
